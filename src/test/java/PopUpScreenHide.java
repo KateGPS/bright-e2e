78475e5473f86309842819bfd3cac263857966fc
KateGPS <e.gandarina@gmail.com>
2022-06-06 09:36:08 +0300

    test: turn off cookies and intercom update test

--- src/test/java/PopUpScreenHide.java
+++ src/test/java/PopUpScreenHide.java
@@ -4,14 +4,9 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
@@ -24,28 +19,36 @@ public class PopUpScreenHide {
         WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeTest
+    @BeforeMethod
     public void openBrowser() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
         driver.get("https://app.neuralegion.com/login");
     }
 
-    @AfterTest
+    @AfterMethod
     public void closeBrowser() {
         driver.quit();
     }
 
-    @Test (description = "turn off cookies and intercome")
-    public void PopUpScreenHide() {
+    @Test (description = "turn off cookies")
+    public void PopUpScreenCookieBannerHide() {
 
-        WebElement cookiesField = driver.findElement(By.cssSelector("body .hs-cookie-notification-position-bottom"));
+        //Act
+        WebElement cookiesBanner = driver.findElement(By.cssSelector("body .hs-cookie-notification-position-bottom"));
         new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.visibilityOf(cookiesField));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].style.visibility='hidden'", cookiesField);
+                .until(ExpectedConditions.visibilityOf(cookiesBanner));
+        //Assert
+        ((JavascriptExecutor) driver).executeScript("arguments[0].style.visibility='hidden'", cookiesBanner);
+    }
 
+    @Test (description = "turn off intercome")
+    public void PopUpScreenIntercomeAppHide() {
 
+        //Act
         WebElement intercomeApp = driver.findElement(By.cssSelector(".mat-typography .intercom-lightweight-app"));
+
+        //Assert
         ((JavascriptExecutor)driver).executeScript("arguments[0].style.visibility='hidden'", intercomeApp);
     }
 }
